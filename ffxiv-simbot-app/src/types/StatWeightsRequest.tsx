e7b0558e97079bfabc6c60ab501d876a551605ac
flyxiv <ns0902@naver.com>
2024-09-14 05:30:03 +0900

    resolved conflict

--- ffxiv-simbot-app/src/types/StatWeightsRequest.tsx
+++ ffxiv-simbot-app/src/types/StatWeightsRequest.tsx
@@ -9,7 +9,3 @@ export interface StatWeightsRequest {
   partyIlvlAdjustment: number;
   usePot: boolean;
 }
-<<<<<<< HEAD
->>>>>>> 15b0829d (added job modified gcds)
-=======
->>>>>>> a1871f3e (configured for electron version)
