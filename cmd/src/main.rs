d1b582c94d3db561d3bc81aca941cda5553f56b4
flyxiv <ns090200@gmail.com>
2024-10-15 04:41:44 +0000

    fixed cmd

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,22 +1,7 @@
-use axum::{
-    handler::HandlerWithoutStateExt,
-    http::{StatusCode, Uri},
-    response::Redirect,
-    routing::get,
-    BoxError, Router,
-};
-use axum_extra::extract::Host;
 use axum_server::tls_rustls::RustlsConfig;
 use ffxiv_simhelper_api::api_server::api_router::create_ffxiv_simhelper_service_router;
 use std::{net::SocketAddr, path::PathBuf};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-#[allow(dead_code)]
-#[derive(Clone, Copy)]
-struct Ports {
-    http: u16,
-    https: u16,
-}
 
 #[tokio::main]
 async fn main() {
@@ -27,11 +12,6 @@ async fn main() {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
-
-    let ports = Ports {
-        http: 3000,
-        https: 13406,
-    };
 
     // configure certificate and private key used by https
     let config = RustlsConfig::from_pem_file(
@@ -44,7 +24,7 @@ async fn main() {
     let app = create_ffxiv_simhelper_service_router();
 
     // run https server
-    let addr = SocketAddr::from(([127, 0, 0, 1], ports.https));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 13406));
     tracing::debug!("listening on {}", addr);
     axum_server::bind_rustls(addr, config)
         .serve(app.into_make_service())
@@ -52,37 +32,4 @@ async fn main() {
         .unwrap();
 }
 
-#[allow(dead_code)]
-async fn redirect_http_to_https(ports: Ports) {
-    fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri, BoxError> {
-        let mut parts = uri.into_parts();
 
-        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
-
-        if parts.path_and_query.is_none() {
-            parts.path_and_query = Some("/".parse().unwrap());
-        }
-
-        let https_host = host.replace(&ports.http.to_string(), &ports.https.to_string());
-        parts.authority = Some(https_host.parse()?);
-
-        Ok(Uri::from_parts(parts)?)
-    }
-
-    let redirect = move |Host(host): Host, uri: Uri| async move {
-        match make_https(host, uri, ports) {
-            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
-            Err(error) => {
-                tracing::warn!(%error, "failed to convert URI to HTTPS");
-                Err(StatusCode::BAD_REQUEST)
-            }
-        }
-    };
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], ports.http));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, redirect.into_make_service())
-        .await
-        .unwrap();
-}
