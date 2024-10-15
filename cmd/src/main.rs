cbaa12c86514a4e7c59a33e4f0fae39a26803551
flyxiv <78268298+flyxiv@users.noreply.github.com>
2024-10-15 12:53:55 +0900

    fixed code

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,105 +1,88 @@
-use axum::{routing::post, Router};
+use axum::{
+    handler::HandlerWithoutStateExt,
+    http::{StatusCode, Uri},
+    response::Redirect,
+    routing::get,
+    BoxError, Router,
+};
+use axum_extra::extract::Host;
+use axum_server::tls_rustls::RustlsConfig;
 use ffxiv_simhelper_api::api_server::api_router::create_ffxiv_simhelper_service_router;
-use hyper::server::conn::Http;
-use hyper::Method;
-use hyper::StatusCode;
-use log::LevelFilter::Info;
-use log::{info, Level, LevelFilter, Metadata, Record, SetLoggerError};
-use std::net::SocketAddr;
-use std::{fs::File, io::BufReader, sync::Arc};
-use tokio_rustls::rustls;
-use tokio_rustls::TlsAcceptor;
-use tower_http::cors::{Any, CorsLayer};
+use std::{net::SocketAddr, path::PathBuf};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-struct SimpleLogger;
-
-impl log::Log for SimpleLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Info
-    }
-
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args());
-        }
-    }
-
-    fn flush(&self) {}
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+struct Ports {
+    http: u16,
+    https: u16,
 }
-
-static LOGGER: SimpleLogger = SimpleLogger;
-pub fn init(log_level: LevelFilter) -> Result<(), SetLoggerError> {
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(log_level))
-}
-
-const PORT_NUMBER: i32 = 13406;
 
 #[tokio::main]
 async fn main() {
-    info!("Loading Logger");
-    init(Info).expect("failed to load logger");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let ports = Ports {
+        http: 3000,
+        https: 13406,
+    };
+
+    // configure certificate and private key used by https
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from("/etc/letsencrypt/live/www.ffxivsimhelper.com/fullchain.pem"),
+        PathBuf::from("/etc/letsencrypt/live/www.ffxivsimhelper.com/privkey.pem"),
+    )
+    .await
+    .unwrap();
 
     let app = create_ffxiv_simhelper_service_router();
 
-    let certs = load_certs("/etc/letsencrypt/live/www.ffxivsimhelper.com/fullchain.pem");
-    let key = load_private_key("/etc/letsencrypt/live/www.ffxivsimhelper.com/privkey.pem");
-
-    let tls_config = rustls::ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_single_cert(certs, key)
-        .expect("bad certificate/key");
-
-    let acceptor = TlsAcceptor::from(Arc::new(tls_config));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 13406));
-
-    let listener = tokio::net::TcpListener::bind(&addr)
-        .await
-        .expect("Failed to bind to address");
-
-    info!("Started Server at port {}", PORT_NUMBER);
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT_NUMBER))
+    // run https server
+    let addr = SocketAddr::from(([127, 0, 0, 1], ports.https));
+    tracing::debug!("listening on {}", addr);
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
         .await
         .unwrap();
+}
 
-    loop {
-        let (stream, _) = listener
-            .accept()
-            .await
-            .expect("Failed to accept connection");
-        let acceptor = acceptor.clone();
-        let app = app.clone();
+#[allow(dead_code)]
+async fn redirect_http_to_https(ports: Ports) {
+    fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri, BoxError> {
+        let mut parts = uri.into_parts();
 
-        tokio::spawn(async move {
-            if let Ok(stream) = acceptor.accept(stream).await {
-                Http::new()
-                    .serve_connection(stream, app.into_make_service())
-                    .await
-                    .expect("failed to serve connection");
-            }
-        });
+        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+
+        if parts.path_and_query.is_none() {
+            parts.path_and_query = Some("/".parse().unwrap());
+        }
+
+        let https_host = host.replace(&ports.http.to_string(), &ports.https.to_string());
+        parts.authority = Some(https_host.parse()?);
+
+        Ok(Uri::from_parts(parts)?)
     }
-}
 
-fn load_certs(path: &str) -> Vec<rustls::Certificate> {
-    let certfile = File::open(path).expect("cannot open certificate file");
-    let mut reader = BufReader::new(certfile);
+    let redirect = move |Host(host): Host, uri: Uri| async move {
+        match make_https(host, uri, ports) {
+            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+            Err(error) => {
+                tracing::warn!(%error, "failed to convert URI to HTTPS");
+                Err(StatusCode::BAD_REQUEST)
+            }
+        }
+    };
 
-    rustls_pemfile::certs(&mut reader)
-        .expect("failed to read certificate")
-        .into_iter()
-        .map(rustls::Certificate)
-        .collect()
-}
-
-fn load_private_key(path: &str) -> rustls::PrivateKey {
-    let keyfile = File::open(path).expect("cannot open private key file");
-    let mut reader = BufReader::new(keyfile);
-
-    let keys = rustls_pemfile::pkcs8_private_keys(&mut reader).expect("failed to read private key");
-
-    assert!(!keys.is_empty(), "failed to load private key");
-
-    rustls::PrivateKey(keys[0].clone())
+    let addr = SocketAddr::from(([127, 0, 0, 1], ports.http));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, redirect.into_make_service())
+        .await
+        .unwrap();
 }
