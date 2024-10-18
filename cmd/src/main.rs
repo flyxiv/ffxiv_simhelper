2f62a076277313ca0fc6d5e16b7cf34c6d14af61
flyxiv <ns090200@gmail.com>
2024-10-18 10:57:34 +0000

    info

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+                .unwrap_or_else(|_| format!("{}=info", env!("CARGO_CRATE_NAME")).into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
