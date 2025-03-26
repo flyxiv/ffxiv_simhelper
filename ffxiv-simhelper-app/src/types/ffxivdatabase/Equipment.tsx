c71363571c75a4310c8c81e00055a421ad0bad4e
flyxiv <78268298+flyxiv@users.noreply.github.com>
2025-03-27 01:14:00 +0900

    added 7.2 equipments (up to 750)

--- ffxiv-simhelper-app/src/types/ffxivdatabase/Equipment.tsx
+++ ffxiv-simhelper-app/src/types/ffxivdatabase/Equipment.tsx
@@ -68,8 +68,8 @@ totalEquipmentsJson.forEach((element) => {
   });
 });
 
-const CURRENT_MIN_ITEM_LEVEL = 690;
-const CURRENT_MAX_ITEM_LEVEL = 735;
+const CURRENT_MIN_ITEM_LEVEL = 710;
+const CURRENT_MAX_ITEM_LEVEL = 750;
 
 export const {
   idDatabase: EQUIPMENT_DATABASE_BY_ID,
